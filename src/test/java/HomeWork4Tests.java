0cdc6b215f8c5bd058c7d992ced30dff44b4cbdb
Maya Romanovich <mariaromanovich@yandex.ru>
2024-09-10 22:42:33 +0300

    added fixes for shouldHave(text

--- src/test/java/HomeWork4Tests.java
+++ src/test/java/HomeWork4Tests.java
@@ -20,6 +20,13 @@ public class HomeWork4Tests {
         open("/selenide/selenide"); // открыли страницу
         $("#wiki-tab").click(); // перешли на вики
         $$(".internal.present").findBy(text("Soft assertions")).click(); // перешли на Soft assertions (проверка действием получается - иначе дальше упадет)
-        $("#user-content-3-using-junit5-extend-test-class").shouldHave(href("#3-using-junit5-extend-test-class")); //проверка
+        $(".highlight.highlight-source-java.notranslate.position-relative.overflow-auto")
+                .shouldHave(text("public class Tests {\n" +
+                "  @Test\n" +
+                "  public void test() {\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}")); //проверка
     }
 }
