85ebd5fa725d20ab84bfb5fbff26b615d19e1386
Maya Romanovich <mariaromanovich@yandex.ru>
2024-09-10 19:29:07 +0300

    added build.gradle + prepare Test

---
+++ src/test/java/HomeWork4Tests.java
@@ -0,0 +1,23 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class HomeWork4Tests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void selenideWikiTest() {
+        open("/selenide/selenide");
+
+    }
+}
