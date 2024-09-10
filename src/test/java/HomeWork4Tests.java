dc837f3bc034267ada1fcb0011ebb91f5f205fd5
Maya Romanovich <mariaromanovich@yandex.ru>
2024-09-10 21:47:51 +0300

    added HomeWork4Tests

--- src/test/java/HomeWork4Tests.java
+++ src/test/java/HomeWork4Tests.java
@@ -2,7 +2,8 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-
+import static com.codeborne.selenide.Condition.href;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 public class HomeWork4Tests {
@@ -12,12 +13,13 @@ public class HomeWork4Tests {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://github.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
     }
 
     @Test
-    void selenideWikiTest() {
-        open("/selenide/selenide");
-
+    void selenideWikiTabTest() {
+        open("/selenide/selenide"); // открыли страницу
+        $("#wiki-tab").click(); // перешли на вики
+        $$(".internal.present").findBy(text("Soft assertions")).click(); // перешли на Soft assertions (проверка действием получается - иначе дальше упадет)
+        $("#user-content-3-using-junit5-extend-test-class").shouldHave(href("#3-using-junit5-extend-test-class")); //проверка
     }
 }
