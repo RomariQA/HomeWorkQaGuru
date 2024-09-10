2c74cefe0b1459b439268f16386affd353a68c94
Maya Romanovich <mariaromanovich@yandex.ru>
2024-09-10 22:43:02 +0300

    added deleted imports

--- src/test/java/HomeWork4Tests.java
+++ src/test/java/HomeWork4Tests.java
@@ -2,7 +2,6 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.href;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
