658347c2c6b3366a51f43679dabb41d9d81d82e1
Maya Romanovich <mariaromanovich@yandex.ru>
2024-09-12 19:08:06 +0300

    fixes for openBrowser

---
+++ src/test/java/HomeWork5Test.java
@@ -0,0 +1,29 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class HomeWork5Test {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void openEnterprize (){
+        String baseUrl = Configuration.baseUrl;
+        open(baseUrl);
+        $(".HeaderMenu-nav").$(byText("Solutions")).hover();
+        $(byLinkText("Enterprise")).click();
+        $("#hero-section-brand-heading").shouldHave(text("""
+                The AI-powered
+                developer platform.
+                """));
+    }
+}
