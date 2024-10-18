a927aaf8d7b156a19526616788494c73bb96c2fd
Maya Romanovich <mariaromanovich@yandex.ru>
2024-10-18 16:20:53 +0300

    added JavaBaseHW

--- src/test/java/pages/JavaBaseHW.java
+++ src/test/java/pages/JavaBaseHW.java
@@ -1,3 +1,5 @@
+package pages;
+
 public class JavaBaseHW {
 
     public static void main(String... args) {
@@ -6,6 +8,12 @@ public class JavaBaseHW {
         byte b = 127;
 
         System.out.println("a + b = " + (byte)(a+b));
+
+        //логические операторы
+        System.out.println("щас мы подтвердим значения а и б - " + (a == 126 && b == 127));
+        System.out.println("щас мы подтвердим значения что б не соответствует вводу- " + (a == 126 && b == 12));
+        System.out.println("щас мы подтвердим что хоть одно из значения истина - " + (a == 126 || b == 22));
+
 
         //вычисления комбинаций типов данных (int и double)
         int c = 10;
